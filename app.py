1543ec4384029840443084137b22b2d48878ed57
Assalli13 <124143755+Assalli13@users.noreply.github.com>
2023-02-13 16:28:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ if __name__ == '__main__':
     #write()
     st.write("Mon application Flask est maintenant compatible avec Streamlit!")
     st.write("Essayez d'interagir avec les widgets ci-dessous.")
-   # app.run()
+    app.run()
