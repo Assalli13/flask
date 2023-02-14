ce6ff5098c6db066c4bd9f46c8ffee510ec0adcf
Assalli13 <124143755+Assalli13@users.noreply.github.com>
2023-02-14 18:03:14 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -8,58 +8,37 @@ import pickle
 
 app = Flask(__name__)
 
-@app.route('/predictByClientId', methods=['POST'])
+@app.route('/')
 def predictByClientId():
     file1 = open('best_model_B_S.pkl', 'rb')
     model = pickle.load(file1)
     file1.close()
-    if model:
-        try:
-            json_ = request.json
-            sample_size = 10000
-            data_set = pd.read_csv('X_test_selected')
-            data_set['SK_ID_CURR'] = data_set['Unnamed: 0.1']
-            client=data_set[data_set['SK_ID_CURR']==json_['SK_ID_CURR']].drop(['Unnamed: 0'],axis=1)
-            y_pred = model.predict(client)
-            y_proba = model.predict_proba(client)
-            #client['TARGET'] = y_pred
-            #print the predicted value and predicted probability
-            
-            print("Predicted value: ", y_pred[0])
-            print("Predicted probability: ", y_proba[0][0])
 
-            #print the predicted value and predicted probability
+    json_ = request.json
+    filename = 'X_test_selected.csv'
+    data_set = pd.read_csv(filename)
+    data_set['SK_ID_CURR'] = data_set['Unnamed: 0.1']
+    #client = data_set[data_set['SK_ID_CURR'] == json_.get('SK_ID_CURR')].drop(['Unnamed: 0'], axis=1) if json_ else None
+    #return jsonify({'error': 'Missing or invalid input data'})
+    #client = data_set[data_set['SK_ID_CURR'] == json_['SK_ID_CURR']].drop(['Unnamed: 0'], axis=1)
+    y_pred = model.predict(data_set.drop(['Unnamed: 0'], axis=1))
+    y_proba = model.predict_proba(data_set.drop(['Unnamed: 0'], axis=1))
 
-            #feature_importances = model.feature_importances_
-            feature_importances = pd.DataFrame(columns=['feature', 'importance'])
-            feature_importances['feature'] = client.columns
-            sum_of_importance = model.feature_importances_.sum()
-            feature_importances['importance'] = model.feature_importances_/sum_of_importance
+    feature_importances = pd.DataFrame(columns=['feature', 'importance'])
+    feature_importances['feature'] = data_set.drop(['Unnamed: 0'], axis=1).columns
+    sum_of_importance = model.feature_importances_.sum()
+    feature_importances['importance'] = model.feature_importances_/sum_of_importance
+    feature_importances_json = feature_importances.to_json(orient='records')
 
-            # Use the to_json method on the DataFrame to convert it to json
-            feature_importances_json = feature_importances.to_json(orient='records')
-            # Display the feature importances
-            #st.write(feature_importances)
-            
-            return jsonify({'prediction': y_pred[0],
-                'prediction_proba':y_proba[0][1],
-                'feature_importances': feature_importances_json})
-   
-
-        except:
-            return jsonify({'trace': traceback.format_exc()})
-    else:
-        print ('Problem loading the model')
-        return ('No model here to use')
-
-@app.route('/')
-def index():
-    return "Welcome to the API"
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(app.root_path,
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    results = {'prediction': y_pred[0], 'prediction_proba':y_proba[0][1], 'feature_importances': feature_importances_json}
+    return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    try:
+        port = int(system.argv[1]) # This is for a command-line input
+    except:
+        port = 12345 # If you don't provide any port the port will be set to 12345
+
+
+    app.run(port=port, debug=True)
+
